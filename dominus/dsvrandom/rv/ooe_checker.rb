1e137d044a4d171f39d5770d7be3027d640a0033
appleforyou <51433264+appleforyou@users.noreply.github.com>
2025-01-16 04:18:50 -0500

    Check glyph sleeve by ID
    There are two glyph sleeve locations, one in normal and one in hard mode.

--- dominus/dsvrandom/rv/ooe_checker.rb
+++ dominus/dsvrandom/rv/ooe_checker.rb
@@ -84,7 +84,7 @@ require 'set'
     #This chest may be randomized in the future but for now it's disabled in normal mode if trick sleeve is on and always glyph sleeve otherwise.
     #In hard mode it would always produce a Trick Sleeve if that option is enabled, so in pickup_randomizer.rb it's set to a non-progression item for that case.
     #if !options[:rv_trick_sleeve]
-      Locations.locs.delete_if {|loc| loc[:name] == "Glyph Sleeve"}
+      Locations.locs.delete_if {|loc| loc[:id] == "02-00-04_07"}
     #end
     @no_progression_locations = Locations.locs.select {|loc| loc[:type].include?("No Progression")}
     @all_droppable_pickups = @all_pickups.select {|key, item| (not item[:progression]) and (not OoEItems.undroppables.has_key?(key))}
