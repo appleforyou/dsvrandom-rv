2aafe7721ec1ec5219a96563397855d163e99a44
appleforyou <51433264+appleforyou@users.noreply.github.com>
2025-01-16 02:22:50 -0500

    Add Trick Sleeve option, remove non-vanilla glyphs
    Non-vanilla glyphs can cause crashes in the Dominus version. Someday I might add this back when I have time to investigate the crashes and fix or avoid them.

--- dominus/dsvrandom/constants/options.rb
+++ dominus/dsvrandom/constants/options.rb
@@ -6,13 +6,13 @@ def self.options
   options = {
   rv_open_castle: "Opens the world map, including access to Dracula's Castle without defeating Barlowe.",
   rv_unlock_albus: "If checked: All villagers will be automatically rescued, which means Albus and Barlowe can be defeated without triggering the Bad Ending.",
-  #rv_non_vanilla_glyphs: "Allows custom glyphs (Albus's gun, and the individual attacks from Arma Felix and Arma Chiroptera) to appear as glyphs in the game. It's unknown if these cause crashes in the Dominus version.",
   rv_unlock_cerberus: "The Cerberus Gate in Dracula's castle will be open without requiring Custos glyphs.",
   rv_split_pools: "If checked, glyphs can only appear at vanilla glyph locations, and items can only appear at vanilla item locations.",
   rv_puzzle_progression: "If checked, progression items can be placed at locations which require certain elemental attack glyphs to solve the puzzle.",
   rv_randomize_quest_rewards: "Quests in Wygol Village which give items will change to give random rewards. Quest requirements do not change. The final quest per villager will give a random glyph, and other quests will give a random equipment or consumable item. Progression glyphs can appear, but they are not required by logic.",
   rv_arthrovertas_revenge: "Tired of not being fought in the randomizer, Arthroverta has decided to take the matter into its own claws... (Pickups will not spawn in the boss's room. Re-enter the room after it dies.)",
   reveal_enemy_info: "Reveals enemy data on the secondary screen which displays enemy HP/EXP/weaknesses. Also will display enemy drops if you have the relic Book of Spirits. Only handles the secondary screen for now, not the bestiary in the pause menu.",
+  rv_trick_sleeve: "Glyph sleeve is made a default feature, and a new sleeve relic is added from a list of random options.",
 
 
   randomize_pickups: "Randomizes items and glyphs you find in static locations.",
